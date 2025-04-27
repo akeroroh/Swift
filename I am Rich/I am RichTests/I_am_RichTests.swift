36e51f7ccf5e79b268c5c4fb7cf6a20864e9106a
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-04-27 22:13:39 +0900

    유데미 2~3강 실습

---
+++ I am Rich/I am RichTests/I_am_RichTests.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_RichTests.swift
+//  I am RichTests
+//
+//  Created by 진아현 on 4/27/25.
+//
+
+import Testing
+@testable import I_am_Rich
+
+struct I_am_RichTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
