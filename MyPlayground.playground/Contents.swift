1abed2132a52b706cb469671aca587bbc7bc77f7
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-05-04 01:52:59 +0900

    유데미 7~12강 실습

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+
+// This is a comment
+
+//print("Hello World")
+
+/* adsfljaldkfj;alkjfd;lakdsjf;lkadf
+ afj;alskdfj
+ ajdf;lkasdf
+ asdjfljasdf
+ skdfjlsdkj*/
+
+print("Hello \(2+3) World")
+
+var a = 5
+var b = 8
+
+var c = 0
+c = a
+a = b
+b = c
+  
+print("The value of a is \(a)")
+print("The value of b is \(b)")
+
