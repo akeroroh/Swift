71f30be595c1817f82a4eec2e5a2d48905a5183e
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-05-04 02:06:20 +0900

    서브모듈 정리

---
+++ BMI-Calculator/BMI Calculator/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by 진아현 on 5/4/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
