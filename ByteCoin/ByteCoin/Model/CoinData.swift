712675ceeb070fda7aa000944a582ce64e240971
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-05-06 01:58:52 +0900

    유데미 14강 실습

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by 진아현 on 5/5/25.
+//  Copyright © 2025 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
