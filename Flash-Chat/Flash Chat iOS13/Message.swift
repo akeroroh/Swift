976b25ce0dc864d692e5cbab7b44dac504b15dbf
AkeroroH <156268545+akeroroh@users.noreply.github.com>
2025-05-11 19:34:46 +0900

    유데미 15강 실습

---
+++ Flash-Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 진아현 on 5/11/25.
+//  Copyright © 2025 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
